e20352f9a546bb0067c361441de9a0de56bdf677
karistyapaicha <karistya0017@gmail.com>
2025-07-13 18:51:17 +0700

    edit style

--- src/components/AddPublicationPage.jsx
+++ src/components/AddPublicationPage.jsx
@@ -96,7 +96,7 @@ export default function AddPublicationPage() {
             id="cover"
             accept="image/*"
             onChange={e => setCoverFile(e.target.files[0])}
-            className="w-full px-3 py-2 bg-gray-100 text-gray-700 border border-gray-300 rounded-md shadow-sm file:bg-white file:border-0 file:px-4 file:py-2 file:rounded file:text-sm file:font-semibold"
+            className="w-full px-3 py-2 border border-black-300 rounded-md bg-gray-100 text-gray-700 border border-gray-300 rounded-md shadow-sm file:bg-white file:border-0 file:px-4 file:py-2 file:rounded file:text-sm file:font-semibold"
           />
         </div>
         <div className="flex justify-end">
